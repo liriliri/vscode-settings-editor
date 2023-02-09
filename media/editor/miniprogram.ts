d1c8bcfd5c1fbe5874c2d390f904a205ed3c1386
redhoodsu <redhoodsu@tencent.com>
2023-02-09 20:06:36 +0800

    chore: small changes

--- media/editor/miniprogram.ts
+++ media/editor/miniprogram.ts
@@ -1,23 +1,42 @@
 import LunaSetting from 'luna-setting'
 import safeSet from 'licia/safeSet'
-import { updateText, i18n } from './util'
-import endWith from 'licia/endWith'
+import { updateText, i18n, buildSettings } from './util'
+import splitPath from 'licia/splitPath'
 
 i18n.set('en', {
+  'miniprogram.appTitle': 'Miniprogram App Config',
+  'miniprogram.appSeeDoc':
+    'Click [here](https://developers.weixin.qq.com/miniprogram/en/dev/reference/configuration/app.html) to see the documentation.',
+  'miniprogram.appEntryPagePathDesc': 'Mini Program default start home page.',
+  'miniprogram.appDebug': 'Whether or not to open debug Mode, off by default.',
+
   'miniprogram.miniappTitle': 'Miniapp Project',
 })
 i18n.set('zh-cn', {
+  'miniprogram.appTitle': '小程序全局配置',
+  'miniprogram.appSeeDoc':
+    '点击[此处](https://developers.weixin.qq.com/miniprogram/dev/reference/configuration/app.html)查看文档。',
+  'miniprogram.appEntryPagePathDesc': '小程序默认启动首页。',
+  'miniprogram.appDebug': '是否开启 debug 模式，默认关闭。',
+
   'miniprogram.miniappTitle': '多端应用',
 })
 
 export function handler(setting: LunaSetting, fileName: string, text: string) {
-  if (endWith(fileName, 'project.config.json')) {
-    project(setting, text)
-    return true
-  } else if (endWith(fileName, 'project.miniapp.json')) {
-    miniapp(setting, text)
-    return true
+  const { name } = splitPath(fileName)
+
+  switch (name) {
+    case 'project.config.json':
+      project(setting, text)
+      return true
+    case 'project.miniapp.json':
+      miniapp(setting, text)
+      return true
+    case 'app.json':
+      app(setting, text)
+      return true
   }
+
   return false
 }
 
@@ -47,6 +66,34 @@ function project(setting: LunaSetting, text: string) {
     json.setting.ignoreUploadUnusedFiles,
     'Ignore unuse files automatically when uploading code.'
   )
+}
+
+function app(setting: LunaSetting, text: string) {
+  const json = JSON.parse(text)
+  setting.on('change', (key, val) => {
+    safeSet(json, key, val)
+
+    updateText(JSON.stringify(json, null, 2) + '\n')
+  })
+
+  buildSettings(setting, [
+    ['title', 'Miniprogram App Config'],
+    ['markdown', i18n.t('miniprogram.appSeeDoc')],
+    [
+      'input',
+      'entryPagePath',
+      json.entryPagePath || '',
+      'Entry Page Path',
+      i18n.t('miniprogram.appEntryPagePathDesc'),
+    ],
+    [
+      'checkbox',
+      'debug',
+      !!json.debug,
+      'Debug',
+      i18n.t('miniprogram.appDebug'),
+    ],
+  ])
 }
 
 function miniapp(setting: LunaSetting, text: string) {
